0a2e66cb05a275ce7f79f7cda485960659dbfde8
Lacey Walker <bluelarksong@gmail.com>
2020-12-03 16:12:19 -0600

    redoing stuff from earlier

---
+++ Roommates34/Models/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Roommates34.Models
+{
+    // C# representation of the Room table
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int MaxOccupancy { get; set; }
+    }
+
+}
